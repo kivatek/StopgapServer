ae02d7b8cc5b796a340d9bac632c64686844d3b9
kivatek <shinya.hrsk@gmail.com>
2015-02-01 17:05:41 +0900

    initial commit.

---
+++ src/main/java/com/kivatek/StopgapServer.java
@@ -0,0 +1,20 @@
+package com.kivatek;
+
+import static spark.Spark.get;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+
+public class StopgapServer {
+
+	public static void main(String[] args) {
+		get("/download/:filename", (request, response) -> {
+			String fn = StringEscapeUtils.unescapeHtml4(request.params("filename"));
+			java.nio.file.Path inputPath = FileSystems.getDefault().getPath("./files", fn);
+			Files.copy(inputPath, response.raw().getOutputStream());
+			return response;
+		});
+	}
+}
