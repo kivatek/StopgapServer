52d770915451bc9b543ac3144d39fdc512b488c4
kivatek <shinya.hrsk@gmail.com>
2016-12-01 00:30:59 +0900

    フレームワークのバージョンアップ 合わせてコードの修正

--- src/main/java/StopgapServer.java
+++ src/main/java/StopgapServer.java
@@ -1,10 +1,13 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static spark.Spark.get;
-import static spark.SparkBase.externalStaticFileLocation;
+import static spark.Spark.staticFiles;
 
 public class StopgapServer {
-
 	public static void main(String[] args) {
-		externalStaticFileLocation("./public");
-		get("/", (request, response) -> "" );
+        Path path = Paths.get("public");
+        staticFiles.externalLocation(path.toAbsolutePath().toString());
+        get("/", (request, response) -> "" );
 	}
 }
